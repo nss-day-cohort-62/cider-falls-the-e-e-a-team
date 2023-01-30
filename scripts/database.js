6364b60166d2a42687a0f962fb94f3a6c05e7a7b
Eric Lehning <ellehning@gmail.com>
2023-01-30 11:40:10 -0600

    added db and mods

---
+++ scripts/database.js
@@ -0,0 +1,117 @@
+const database = {
+    employees: [{
+        id: 1,
+        name: "Alphonse Meron",
+        parkId: 
+
+    }, {
+        id: 2,
+        name: "Damara Pentecust",
+        parkId:
+
+    }, {
+        id: 3,
+        name: "Anna Bowton",
+        parkId:
+
+    }, {
+        id: 4,
+        name: "Hunfredo Drynan",
+        parkId:
+
+    }, {
+        id: 5,
+        name: "Elmira Bick",
+        parkId:
+
+    }, {
+        id: 6,
+        name: "Bernie Dreger",
+        parkId:
+
+    }, {
+        id: 7,
+        name: "Rolando Gault",
+        parkId:
+
+    }, {
+        id: 8,
+        name: "Tiffanie Tubby",
+        parkId:
+
+    }, {
+        id: 9,
+        name: "Tomlin Cutill",
+        parkId:
+
+    }, {
+        id: 10,
+        name: "Arv Biddle",
+        parkId:
+
+    }, {
+        id: 11,
+        name: "Marvin Hamlish",
+        parkId:
+    }, {
+        id: 12,
+        name: "Alfred Prufroct",
+        parkId:
+    }],
+    services: [{
+        id: 1,
+        name: "Rafting",
+        parkIds: 
+    }, {
+        id: 2,
+        name: "Canoeing",
+        parkIds: 
+    }, {
+        id: 3,
+        name: "Fishing",
+        parkIds: 
+    }, {
+        id: 4,
+        name: "Hiking",
+        parkIds: 
+    }, {
+        id: 5,
+        name: "Picnicking",
+        parkIds: 
+    }, {
+        id: 6,
+        name: "Rock climbing",
+        parkIds: 
+    }, {
+        id: 7,
+        name: "Lodging",
+        parkIds: 
+    }, {
+        id: 8,
+        name: "Restaurant",
+        parkIds: 
+    }, {
+        id: 9,
+        name: "Parking",
+        parkIds: 
+    }, {
+        id: 10,
+        name: "Information",
+        parkIds: 
+    }, {
+        id: 11,
+        name: "Office",
+        parkIds: 
+    }, {
+        id: ,
+        name: ,
+        parkIds: 
+    }, {
+        id: ,
+        name: ,
+        parkIds: 
+    }],
+    parks: [{
+
+    }]
+}
