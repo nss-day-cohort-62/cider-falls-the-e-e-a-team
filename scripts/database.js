4cb270f8e5054d2e06a4a7ecfaccbf8f8c8a9ef7
Eric Lehning <ellehning@gmail.com>
2023-01-30 13:25:14 -0600

    db completed

--- scripts/database.js
+++ scripts/database.js
@@ -1,117 +1,125 @@
 const database = {
-    employees: [{
+    guests: [{
         id: 1,
         name: "Alphonse Meron",
-        parkId: 
-
     }, {
         id: 2,
         name: "Damara Pentecust",
-        parkId:
-
     }, {
         id: 3,
         name: "Anna Bowton",
-        parkId:
-
     }, {
         id: 4,
         name: "Hunfredo Drynan",
-        parkId:
-
     }, {
         id: 5,
         name: "Elmira Bick",
-        parkId:
-
     }, {
         id: 6,
         name: "Bernie Dreger",
-        parkId:
-
     }, {
         id: 7,
         name: "Rolando Gault",
-        parkId:
-
     }, {
         id: 8,
         name: "Tiffanie Tubby",
-        parkId:
-
     }, {
         id: 9,
         name: "Tomlin Cutill",
-        parkId:
-
     }, {
         id: 10,
         name: "Arv Biddle",
-        parkId:
-
     }, {
         id: 11,
-        name: "Marvin Hamlish",
-        parkId:
+        name: "Marvin Hamlish", 
     }, {
         id: 12,
-        name: "Alfred Prufroct",
-        parkId:
+        name: "Alfred Prufroct"
     }],
     services: [{
         id: 1,
         name: "Rafting",
-        parkIds: 
+        parkIds: [1] 
     }, {
         id: 2,
         name: "Canoeing",
-        parkIds: 
+        parkIds: [1] 
     }, {
         id: 3,
         name: "Fishing",
-        parkIds: 
+        parkIds: [1, 4] 
     }, {
         id: 4,
         name: "Hiking",
-        parkIds: 
+        parkIds: [2, 4, 6] 
     }, {
         id: 5,
         name: "Picnicking",
-        parkIds: 
+        parkIds: [2, 3, 6] 
     }, {
         id: 6,
         name: "Rock climbing",
-        parkIds: 
+        parkIds: [2] 
     }, {
         id: 7,
         name: "Lodging",
-        parkIds: 
+        parkIds: [3, 5] 
     }, {
         id: 8,
-        name: "Restaurant",
-        parkIds: 
+        name: "Food",
+        parkIds: [3, 6] 
     }, {
         id: 9,
         name: "Parking",
-        parkIds: 
+        parkIds: [3, 5] 
     }, {
         id: 10,
         name: "Information",
-        parkIds: 
+        parkIds: [3, 5] 
     }, {
         id: 11,
-        name: "Office",
-        parkIds: 
+        name: "Chidren play areas",
+        parkIds: [5] 
     }, {
-        id: ,
-        name: ,
-        parkIds: 
-    }, {
-        id: ,
-        name: ,
-        parkIds: 
+        id: 12,
+        name: "Zipline",
+        parkIds: [6] 
     }],
     parks: [{
+        id: 1,
+        name: "Chamfort River",
+        serviceIds: [1, 2, 3],
+        guestIds: [1, 2]
+
+    }, {
+        id: 2,
+        name: "Lost Wolf Hiking Trail",
+        serviceIds: [4, 5, 6],
+        guestIds: [3, 4]
+
+    }, {
+        id: 3,
+        name: "Lodge",
+        serviceIds: [5, 7, 8, 9, 10],
+        guestIds: [5, 6]
+
+    }, {
+        id: 4,
+        name: "Gander River",
+        serviceIds: [3, 4],
+        guestIds: [7, 8]
+
+    }, {
+        id: 5,
+        name: "Campgrounds",
+        serviceIds: [7, 9, 10, 11],
+        guestIds: [9, 10]
+
+    }, {
+        id: 6,
+        name: "Pine Bluffs Trail",
+        serviceIds: [4, 5, 8, 12],
+        guestIds: [11, 12]
 
     }]
 }
